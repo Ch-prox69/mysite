36be25d9002203bfa4b9c92911dd3b5c8e598bd9
Christian Hartzell <christian.hartzell@fwcsstudents.org>
2025-04-28 18:14:48 -0400

    Push 4: Base, Index, urls, and views complete

--- main/views.py
+++ main/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
-def home(request):
-    return HttpResponse("This is the homepage.")
+def index(request):
+    return render(request, 'main/index.html')
