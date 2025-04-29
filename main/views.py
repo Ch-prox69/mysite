e4d573a9d65d48503d2e47d93c1d2ece3d814ff3
Christian Hartzell <christian.hartzell@fwcsstudents.org>
2025-04-29 13:39:53 -0400

    Updated Home, and About Page

--- main/views.py
+++ main/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
 
+# Index page view
 def index(request):
     return render(request, 'index.html')
+
+# About page view
+def about(request):
+    return render(request, 'about.html')  # This points to the 'about.html' template
