e44e980d7825397c2a5647e0675e6f3b281d645d
Christian Hartzell <christian.hartzell@fwcsstudents.org>
2025-05-06 12:03:51 -0400

    Added admin to answer questions

--- questions/admin.py
+++ questions/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Question
 
-# Register your models here.
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('name', 'submitted_at', 'answer_text')
+    search_fields = ('name', 'question_text', 'answer_text')
